0b8a7961e6777823c77f334261673fe5d129fbc3
Craig Nicol <craig.nicol@validusrm.com>
2022-07-08 16:57:24 +0100

    coroutines run and gather

---
+++ asink/gather_coroutines.py
@@ -0,0 +1,30 @@
+"""Define methods which return coroutines that can be awaited or gathered."""
+import asyncio
+import typing
+
+
+# Define methods using the async keyword to have them return coroutines.
+async def generate_coro_one() -> asyncio.coroutine:
+    print("Start coro_one.")
+    await asyncio.sleep(2)
+    print("End coro_one.")
+    return "Returned from coro_one"
+
+async def generate_coro_two() -> asyncio.coroutine:
+    print("Start coro_two.")
+    await asyncio.sleep(1)
+    print("Mid coro_two.")
+    await asyncio.sleep(1)
+    print("End coro_two.")
+    return "Returned from coro_two"
+
+async def gather_the_coros(coros: typing.List[asyncio.coroutine]) -> asyncio.coroutine:
+    gatherings = await asyncio.gather(*coros)
+    return gatherings
+
+coros = [gen_coro() for gen_coro in [generate_coro_one, generate_coro_two]]
+gatherings = gather_the_coros(coros)
+coro_returns = asyncio.run(gatherings)
+
+for coro_return in coro_returns:
+    print(coro_return)
---
+++ asink/single_coroutine.py
@@ -0,0 +1,15 @@
+"""Define methods which return coroutines that can be awaited or gathered."""
+import asyncio
+
+
+# Define a method using the async keyword to have it return a coroutine.
+async def generate_coro_one() -> asyncio.coroutine:
+    print("Start coro_one.")
+    await asyncio.sleep(2)
+    print("End coro_one.")
+    return "This is what will be returned when the coroutine is given to asyncio.run"
+
+# Execute the generate_coro_one to get a coroutine object, run this using asyncio.run.
+coro_one = generate_coro_one()
+something = asyncio.run(coro_one)
+print(something)
