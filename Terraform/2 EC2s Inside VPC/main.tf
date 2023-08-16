016b535305fae5d146f28d426cf626944430d21b
Craig Nicol <cm.nicol@outlook.com>
2023-08-16 13:07:16 +0100

    2 EC2s inside VPC Terraform

---
+++ Terraform/2 EC2s Inside VPC/main.tf
@@ -0,0 +1,41 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.2.1"
+  count   = 2
+
+  name = "my-ec2-cluster-${count.index}"
+
+  ami                    = "ami-0ed752ea0f62749af"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
---
+++ Terraform/2 EC2s Inside VPC/outputs.tf
@@ -0,0 +1,20 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+output "vpc_public_subnets" {
+  description = "IDs of the VPC's public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "ec2_instance_public_ips" {
+  description = "Public IP addresses of EC2 instances"
+  value       = module.ec2_instance[*].public_ip
+}
+
+
+output "ec2_instance_ids" {
+  description = "Public IP addresses of EC2 instances"
+  value       = module.ec2_instance[*].id
+}
+
+
